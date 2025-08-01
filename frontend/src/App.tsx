2d0cd25f8b8417090ccf81b39cd6dbac61e1ccc6
Dhawanit Bhatnagar <dhawanitbhatnagar@Dhawanits-MacBook-Air.local>
2025-08-01 12:06:15 +0530

    Document Module Enhancements, Ingestion Module Integration has been done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import LoginPage from './pages/Auth/LoginPage';
 import DashboardPage from './pages/Dashboard/DashboardPage';
 import DocumentsPage from './pages/Documents/DocumentsPage';
 import UsersPage from './pages/Users/UsersPage';
-// import IngestionPage from './pages/Ingestion/IngestionPage';
+import IngestionPage from './pages/Ingestion/IngestionPage';
 import ProtectedRoute from './routes/ProtectedRoute';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/documents" element={<DocumentsPage />} />
-        {/* <Route path="/ingestion" element={<IngestionPage />} /> */}
+        <Route path="/ingestion" element={<IngestionPage />} />
       </Route>
 
       {/* Admin-only */}
